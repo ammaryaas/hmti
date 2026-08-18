794a60b6fb6d094669b27e9fe5ca44526f93497b
AthayaRaihan <athayaraihanannafi@gmail.com>
2026-08-18 16:31:58 +0700

    update corner (berita, apresiasi), update detail news

--- resources/views/apresiasi.blade.php
+++ resources/views/apresiasi.blade.php
@@ -1,28 +1,19 @@
 <x-layouts.app 
-	title="Corner - HMTI" 
+	title="Apresiasi - HMTI" 
 	body-class="text-slate-900 antialiased bg-[#d8d3d3]" 
 	active-nav="corner">
 
     <section class="bg-[#d8d3d3] py-12 lg:py-16">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="mb-10 border-b-[5px] border-[#817272] pb-2 inline-block">
-                <h1 class="text-4xl font-bold italic text-black sm:text-5xl">
-                    Berita Terbaru
+                <h1 class="text-4xl font-bold italic text-black sm:text-5xl uppercase tracking-wider">
+                    APPRECIATION
                 </h1>
             </div>
 
             <div class="rounded-xl bg-[#F2F2F2] p-6 shadow-sm lg:p-10">
-                <livewire:corner-news />
+                <livewire:appreciation-list />
             </div>
-
-            <style>
-                .news-title-clamp {
-                    display: -webkit-box;
-                    -webkit-line-clamp: 2;
-                    -webkit-box-orient: vertical;
-                    overflow: hidden;
-                }
-            </style>
 
         </div>
     </section>
